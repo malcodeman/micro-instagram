41d6e12c68c163c9f94b24011e84fd17138cb5c3
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-26 19:20:17 +0200

    Adds posts scrapping

--- src/users.py
+++ src/users.py
@@ -8,6 +8,15 @@ def scrap_page(page):
     return result.json()
 
 
+def get_posts(json_content):
+    posts_array = []
+    for post in json_content["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]:
+        posts_array.append({
+            "pic_url": post["node"]["display_url"]
+        })
+    return posts_array
+
+
 def get_user_info(json_content):
     user = {
         "biography": json_content["graphql"]["user"]["biography"],
@@ -22,6 +31,7 @@ def get_user(user):
     json_content = scrap_page(
         "https://www.instagram.com/" + str(user) + "/?__a=1")
     response = {
-        "user": get_user_info(json_content)
+        "user": get_user_info(json_content),
+        "posts": get_posts(json_content)
     }
     return response
