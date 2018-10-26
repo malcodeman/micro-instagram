50be9830060d4e685d787bf72dcbee5bacdf1427
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-26 17:59:07 +0200

    Adds user info scrapping

--- src/users.py
+++ src/users.py
@@ -1,7 +1,27 @@
-from flask import request
-from bs4 import BeautifulSoup
 import requests
+import os
+
+
+def scrap_page(page):
+    headers = {"Cookie": os.environ['COOKIE']}
+    result = requests.get(page, headers=headers)
+    return result.json()
+
+
+def get_user_info(json_content):
+    user = {
+        "biography": json_content["graphql"]["user"]["biography"],
+        "posts": json_content["graphql"]["user"]["edge_owner_to_timeline_media"]["count"],
+        "followers": json_content["graphql"]["user"]["edge_followed_by"]["count"],
+        "following": json_content["graphql"]["user"]["edge_follow"]["count"],
+    }
+    return user
 
 
 def get_user(user):
-    return user
+    json_content = scrap_page(
+        "https://www.instagram.com/" + str(user) + "/?__a=1")
+    response = {
+        "user": get_user_info(json_content)
+    }
+    return response
