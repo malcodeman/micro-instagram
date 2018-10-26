3030edf33ad1e6649b68b9718d842f5908b2ad00
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-26 16:31:02 +0200

    Adds basic setup

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import abort
+
+import users
+
+app = Flask(__name__)
+
+
+@app.route("/users/<string:user>")
+def get_user(user):
+    return jsonify(users.get_user(user))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/users.py
@@ -0,0 +1,7 @@
+from flask import request
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_user(user):
+    return user
