8d60d4ff91e1f5d220850375890cbde8f3e475f9
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-27 18:55:16 +0200

    Adds next 12 posts fetching

--- src/users.py
+++ src/users.py
@@ -3,18 +3,60 @@ import os
 
 
 def scrap_page(page):
-    headers = {"Cookie": os.environ['COOKIE']}
+    headers = os.environ['COOKIE']
     result = requests.get(page, headers=headers)
     return result.json()
 
 
-def get_posts(json_content):
+def find_query_id_index(content):
+    index1 = content.find("queryId")
+    index2 = content.find("queryId", index1 + 9)
+    return content.find("queryId", index2 + 9) + 9
+
+
+def get_query_id(content):
+    index = find_query_id_index(content)
+    queryId = content[index:index + 32]
+    return queryId
+
+
+def scrap_js(page_url):
+    result = requests.get(page_url)
+    return result.text
+
+
+def get_next_page(channel_id, query_id, end_cursor):
+    headers = os.environ['COOKIE']
+    params = f'query_hash={query_id}&variables={{"id": "{channel_id}", "first": 12, "after": "{end_cursor}"}}'
+    result = requests.get(
+        f'https://www.instagram.com/graphql/query/?{params}',  headers=headers)
+    return result.json()
+
+
+def get_posts_next(json_content):
+    content = json_content["data"]["user"]["edge_owner_to_timeline_media"]
     posts_array = []
-    for post in json_content["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]:
+    for post in content["edges"]:
         posts_array.append({
             "pic_url": post["node"]["display_url"]
         })
     return posts_array
+
+
+def get_posts(json_content, query_id):
+    content = json_content["graphql"]["user"]["edge_owner_to_timeline_media"]
+    next_page = content["page_info"]["has_next_page"]
+    end_cursor = content["page_info"]["end_cursor"]
+    channel_id = json_content["graphql"]["user"]["id"]
+    posts_latest = []
+    for post in content["edges"]:
+        posts_latest.append({
+            "pic_url": post["node"]["display_url"]
+        })
+    posts_next = get_posts_next(
+        get_next_page(channel_id, query_id, end_cursor))
+    merged_posts = posts_latest + posts_next
+    return merged_posts
 
 
 def get_user_info(json_content):
@@ -28,10 +70,12 @@ def get_user_info(json_content):
 
 
 def get_user(user):
+    url = "https://www.instagram.com/static/bundles/base/ProfilePageContainer.js/1ead5e8e1146.js"
+    query_id = get_query_id(scrap_js(url))
     json_content = scrap_page(
         "https://www.instagram.com/" + str(user) + "/?__a=1")
     response = {
         "user": get_user_info(json_content),
-        "posts": get_posts(json_content)
+        "posts": get_posts(json_content, query_id)
     }
     return response
